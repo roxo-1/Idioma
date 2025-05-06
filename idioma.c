81de607ca493fc1503c84eff56e8bcf71cc27f74
roxo-1 <carolzinhalee09@gmail.com>
2025-05-05 22:45:26 -0300

    Input

---
+++ idioma.c
@@ -0,0 +1,20 @@
+// Carolina Lee - 10440304
+#include <stdio.h>
+
+int main() {
+    #include <stdio.h>
+
+int main() {
+    char texto[100];  // Vetor para armazenar até 99 caracteres + '\0'
+
+    printf("Digite um texto: ");
+    fgets(texto, sizeof(texto), stdin);  // Lê uma linha de texto com espaços
+
+    printf("Você digitou: %s", texto);
+
+    return 0;
+}
+
+    
+    return 0;
+}
